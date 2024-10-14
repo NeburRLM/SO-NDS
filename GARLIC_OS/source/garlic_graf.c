73c37fe179538b3460d47d0142adb3af31516073
Eros Vilar <eros.vilar@estudiants.urv.cat>
2024-10-14 20:28:58 +0200

    progG: Implementacio _gg_iniGrafA()

--- GARLIC_OS/source/garlic_graf.c
+++ GARLIC_OS/source/garlic_graf.c
@@ -10,7 +10,19 @@
 #include "garlic_system.h"	// definición de funciones y variables de sistema
 #include "garlic_font.h"	// definición gráfica de caracteres
 
+/* definiciones para realizar cálculos relativos a la posición de los
+caracteres dentro de las ventanas gráficas, que pueden ser 4 o 16 */
+#define NVENT 4 // número de ventanas totales
+#define PPART 2 // número de ventanas horizontales
 
+// (particiones de pantalla)
+#define VCOLS 32 // columnas y filas de cualquier ventana
+#define VFILS 24
+#define PCOLS VCOLS * PPART // número de columnas totales
+#define PFILS VFILS * PPART // número de filas totales
+
+// Variables
+int background_2, background_3;
 
 /* _gg_generarMarco: dibuja el marco de la ventana que se indica por parámetro*/
 void _gg_generarMarco(int v)
@@ -22,7 +34,56 @@ void _gg_generarMarco(int v)
 /* _gg_iniGraf: inicializa el procesador gráfico A para GARLIC 1.0 */
 void _gg_iniGrafA()
 {
+	videoSetMode(MODE_5_2D);	// Inicialitzar processador gràfic principal (A) en mode 5
+	lcdMainOnTop();				// Establir pantalla superior com a principal
+	
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);	// Reservar banc de memòria de video A
 
+	/* Inicialitzar fons grafic 2
+	 * ---
+	 * Especificacions generals:
+	 * Mida mapa = 32x32 posiciones * 2 bytes/posición = 2KB
+	 * Mida baldoses = 128 baldosas * 8x8 píxeles/baldosa * 1 byte/píxel = 8KB
+	 * Dir.ini mapa (norma) = VirtVRAM_Background + mapBase * 2Kbytes
+	 * Dir.ini baldoses = VirtVRAM_Background + tileBase * 16 Kbytes
+	 * ---
+	 * Dir.ini mapa = 0x06000000 + 0 * 2KB = 0x06000000 -> Inicialitzem al principi del banc de memoria
+	 * Dir.ini baldoses = 0x06000000 + 1 * 16KB = 0x06016384 -> Direccio inical baldoses
+	*/
+	background_2 = bgInit(2, BgType_ExRotation, BgSize_ER_512x512, 0, 1);
+	
+	/* Inicialitzar fons grafic 3
+	 * Dir.ini mapa = 0x06000000 + 2 * 2KB = 0x06004096
+	 * Dir.ini baldoses = 0x06000000 + 1 * 16KB = 0x06016384 -> Direccio inical baldoses
+	*/
+	background_3 = bgInit(3, BgType_ExRotation, BgSize_ER_512x512, 2, 1);
+
+	// Prioritzar fons 3 > 2
+	bgSetPriority(background_2, 1);
+	bgSetPriority(background_3, 0);
+	
+	// Descomprimir el contingut de la font de lletres -> garlic_fontTiles (garlic_font.s)
+	decompress(garlic_fontTiles, bgGetGfxPtr(background_2), LZ77Vram);
+	
+	// Copiar la paleta de colors
+	dmaCopy(garlic_fontPal, BG_PALETTE, sizeof(garlic_fontPal));
+
+	// Generar els marcos de les finestres de text del fons 3
+	for (int i=0; i<NVENT; i++)
+	{
+		_gg_generarMarco(i);
+	}
+
+	/* Escalar fons 2 i 3
+	 * Los parámetros de escalado se tienen que proporcionar en formato de coma fija 0.24.8.
+	 * 50% = 0b0101 = 0x5
+	 * Format coma fixa (0.24.8) = 0x00000005
+	*/
+	bgSetScale(background_2, 0x00000005, 0x00000005);
+	bgSetScale(background_3, 0x00000005, 0x00000005);
+	
+	// Actualiza todos los fondos despues de realizar rotaciones, escalados o desplazamientos
+	bgUpdate();
 }
 
 
